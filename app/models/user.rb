35c872c59f64e544aaf188860aa409c7b38679c1
長野汐央里 <>
2024-09-24 12:56:48 +0900

    ユーザー認証機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@
 # Table name: users
 #
 #  id                     :bigint           not null, primary key
-#  account                :string           default(""), not null
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
+#  username               :string           default(""), not null
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #
@@ -22,4 +22,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2024_09_21_125043) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "account", default: "", null: false
+    t.string "username", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
