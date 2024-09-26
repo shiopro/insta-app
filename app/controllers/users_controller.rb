ca5f1a7760b8d47ad20bf7922fd602811f28bba9
長野汐央里 <>
2024-09-27 01:47:39 +0900

    プロフィール画像をクリックしたら画像選択と保存ができるようにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,19 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: 'プロフィールが更新されました'
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:avatar)
+  end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,20 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+document.addEventListener("DOMContentLoaded", function() {
+  const profileIcon = document.querySelector('.profile_icon');
+  const fileField = document.getElementById('avatar_field');
+  const form = profileIcon.closest('form');//フォーム要素を取得
 
+  profileIcon.addEventListener('click', function() {
+    fileField.click();// ファイル選択ダイアログを開く
+  });
+  fileField.addEventListener('change', function(){
+    if (fileField.files.length > 0) {
+      form.submit();
+    }
+  });
+});
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
